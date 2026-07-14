9d04db29559276e0da112cc72b31c6ff7a201638
Пользователь <abduhamidovm52@gmail.com>
2026-07-14 23:50:15 +0500

    Use Vercel Blob

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -2,10 +2,11 @@ import { ConnectDB } from "@/lib/config/db"
 import BlogModel from "@/lib/models/BlogModel";
 
 const { NextResponse } = require("next/server")
-import {writeFile} from 'fs/promises'
-import { title } from "process";
-const fs = require('fs')
+// import {writeFile} from 'fs/promises'
+// import { title } from "process";
+// const fs = require('fs')
 
+import { put, del } from "@vercel/blob";
 
 const LoadDB = async () => {
     await ConnectDB(); 
@@ -29,38 +30,87 @@ export async function GET(request){
 
 
 // Api Endpoint for Uploading Blogs
-export async function POST(request){
-    const formData = await request.formData();
-    const timestamp = Date.now();
 
-    const image = formData.get('image');
-    const imageByteData = await image.arrayBuffer();
-    const buffer = Buffer.from(imageByteData);
-    const path = `./public/${timestamp}_${image.name}`;
-    await writeFile(path, buffer);
-    const imgUrl = `/${timestamp}_${image.name}`;
+// export async function POST(request){
+//     const formData = await request.formData();
+//     const timestamp = Date.now();
+
+//     const image = formData.get('image');
+//     const imageByteData = await image.arrayBuffer();
+//     const buffer = Buffer.from(imageByteData);
+//     const path = `./public/${timestamp}_${image.name}`;
+//     await writeFile(path, buffer);
+//     const imgUrl = `/${timestamp}_${image.name}`;
     
-    const blogData ={
-        title:`${formData.get('title')}`,
-        description:`${formData.get('description')}`,
-        category:`${formData.get('category')}`,
-        author: `${formData.get('author')}`,
-        image: `${imgUrl}`,
-        authorImg: `${formData.get('authorImg')}`
-    }
+//     const blogData ={
+//         title:`${formData.get('title')}`,
+//         description:`${formData.get('description')}`,
+//         category:`${formData.get('category')}`,
+//         author: `${formData.get('author')}`,
+//         image: `${imgUrl}`,
+//         authorImg: `${formData.get('authorImg')}`
+//     }
+
+//     await BlogModel.create(blogData);
+//     console.log("Blog Saved")
+
+//     return NextResponse.json({success:true, msg:"Blog Added"})
+// }
+
+export async function POST(request){
+
+    const formData = await request.formData();
+
+    const image = formData.get("image");
+
+    const blob = await put(`${Date.now()}-${image.name}`, image, {
+        access: "public",
+    });
+
+    const blogData = {
+
+        title: formData.get("title"),
+        description: formData.get("description"),
+        category: formData.get("category"),
+        author: formData.get("author"),
+        image: blob.url,
+        authorImg: formData.get("authorImg")
+
+    };
 
     await BlogModel.create(blogData);
-    console.log("Blog Saved")
 
-    return NextResponse.json({success:true, msg:"Blog Added"})
+    return NextResponse.json({
+        success:true,
+        msg:"Blog Added"
+    });
+
 }
-
 // Creating Api endpoint to delete Blog
 
-export async function DELETE (request){
-    const id = await request.nextUrl.searchParams.get('id');
+// export async function DELETE (request){
+//     const id = await request.nextUrl.searchParams.get('id');
+//     const blog = await BlogModel.findById(id);
+//     fs.unlink(`./public${blog.image}`, () =>{});
+//     await BlogModel.findByIdAndDelete(id);
+//     return NextResponse.json({msg:"Blog Deleted"})
+// }
+
+export async function DELETE(request){
+
+    const id = request.nextUrl.searchParams.get("id");
+
     const blog = await BlogModel.findById(id);
-    fs.unlink(`./public${blog.image}`, () =>{});
+
+    if(blog.image){
+        await del(blog.image);
+    }
+
     await BlogModel.findByIdAndDelete(id);
-    return NextResponse.json({msg:"Blog Deleted"})
+
+    return NextResponse.json({
+        success:true,
+        msg:"Blog Deleted"
+    });
+
 }
